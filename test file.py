540542685c2456cde0d8fc7d08a86ada6f47fa2f
chellick <matsupus@gmail.com>
2023-01-09 16:35:58 +0300

    1

--- test file.py
+++ test file.py
@@ -612,7 +612,7 @@
 # lst_fit_temp_y = []
 # lst_fit_temp_z = []
 # best_individ_y = []
-# best_individ_x = []
+# best_individ_x_one = []
 # best_individ_z = []
 # best_y = -math.inf
 # best_z = -math.inf
@@ -780,7 +780,7 @@
 #
 #
 # for i in best_individ_y:
-#     best_individ_x.append(float_number(i))
+#     best_individ_x_one.append(float_number(i))
 #
 #
 # # >--------------------------------------------------------------------------------------------------------
@@ -790,7 +790,7 @@
 # # >--------------------------------------------------------------------------------------------------------
 #
 # ax = plt.axes(projection="3d")
-# x = best_individ_x
+# x = best_individ_x_one
 # y = best_individ_array_y
 # z = best_individ_array_z
 # ax.scatter(x, y, z)
@@ -803,7 +803,7 @@
 # # figure, axis = plt.subplots()
 # # x_one = np.linspace(limit_one, limit_two, 50)
 # # y_one = [function(i) for i in x_one]
-# # x = best_individ_x
+# # x = best_individ_x_one
 # # y = best_individ_array_y
 # #
 # # plt.scatter(x, y, 20)
@@ -841,7 +841,7 @@
 # lst_fit_x = []
 # lst_fit_temp = []
 # best_individ = []
-# best_individ_x = []
+# best_individ_x_one = []
 # best_individ_x2 = []
 #
 # limit_one = int(input("ограничение 1 "))
@@ -964,7 +964,7 @@
 #
 #
 # for i in best_individ:
-#     best_individ_x.append(fitness_x(i))
+#     best_individ_x_one.append(fitness_x(i))
 #
 # for i in best_individ:
 #     best_individ_x2.append(fitness_x2(i))
@@ -978,7 +978,7 @@
 #
 #
 # ax = plt.axes(projection="3d")
-# x = best_individ_x
+# x = best_individ_x_one
 # y = best_individ_x2
 # z = best_p
 # ax.scatter(x, y, z)
@@ -990,7 +990,7 @@
 # # figure, axis = plt.subplots()
 # # x_one = np.linspace(limit_one, limit_two, 50)
 # # y_one = [fi(i) for i in x_one]
-# # x = best_individ_x
+# # x = best_individ_x_one
 # # y = best_p
 # #
 # # plt.scatter(x, y, 20)
@@ -999,72 +999,90 @@
 # # print(x , 'x')
 # # print(y, 'y')
 
-
+# from calendar import c
+# from os import sep
 
 
 import math
 from random import randint, random
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
-temp_count_fit = 0
-child = []
+
+
+temp_count_fit = 0                    # Среднее значение float в итерации
+child = []                            # Ребёнок
 # count = 0
 # count2 = 0
-best = 0
-parent_one = []
-parent_two = []
-population = []
-countc = 0
+best = 0                              # Сравнительная переменная (min\max)
+parent_one = []                       # 1 Участник в генерации потомка
+parent_two = []                       # 2 Участник в генерации потомка
+population = []                       # Оснвной массив популяции
+# countc = 0
 count_str = []
-individ = int(input("сколько хотите индивидов?  "))
+count_individ = int(input("сколько хотите индивидов?  "))
 len_individ = int(input("длина индивидов  "))
-steps = int(input("кол-во поколений  "))
-mutation_veriety = float(input("вероятность мутации   "))
-sep_population = []
+count_generations = int(input("кол-во поколений  "))
+mutation_probability = float(input("вероятность мутации   "))
+child_population = []
 count_str_x = []
-best_p = []
-best_count = 0
+best_individ_array = []
+# best_count = 0
 population_fit = []
 lst_fit_x = []
 lst_fit_temp = []
 best_individ = []
-best_individ_x = []
+best_individ_x_one = []
+best_individ_x_two = []
 limit_one = int(input("ограничение 1 "))
 limit_two = int(input("ограничение 2 "))
 best = -math.inf
 
 # >--------------------------------------------------------------------------------------------------------
+def function(x1, x2):
+    return -(x1 ** 2 + x2 ** 2)                                 # Задание фунции
 
-def fi(x):
-    return -(x-2)**2
+# def function(x):
+#     return -(x-2)**2
 
-def fitness(indiv):
-    s_individ = "".join(map(str, indiv))
-    int_individ = int(s_individ, 2)
-    float_individ = (int_individ / (2 ** len(indiv) - 1)) * (limit_two - limit_one) + limit_one
-    float_individ_y = fi(float_individ)
-    # print(float_individ, float_individ_y)
+def fitness_one(indiv):                                         # Значение Y (Z)
+    s_indiv = "".join(map(str, indiv))
+    int_individ_one = int(s_indiv[:len(s_indiv) // 2 + 1], 2)
+    int_individ_two = int(s_indiv[len(s_indiv) // 2:], 2)
+
+    float_individ_one = (int_individ_one / (2 ** (len(indiv) / 2) - 1)) * (limit_two - limit_one) + limit_one
+    float_individ_two = (int_individ_two / (2 ** (len(indiv) / 2) - 1)) * (limit_two - limit_one) + limit_one  # Значение
+
+    # print(float_individ_one, float_individ_two)
+
+    float_individ_y = function(float_individ_one, float_individ_two)
     return float_individ_y
 
-def fitness_x(indiv):
+def float_number(indiv):                                        # Tuple X1, X2 (X, Y)
     s_indiv = "".join(map(str, indiv))
-    int_individ = int(s_indiv, 2)
-    float_individ = (int_individ / (2 ** len(indiv) - 1)) * (limit_two - limit_one) + limit_one
-    return float_individ
+    int_individ_one = int(s_indiv[:len(s_indiv) // 2 + 1], 2)
+    int_individ_two = int(s_indiv[len(s_indiv) // 2:], 2)
+
+    float_individ_one = (int_individ_one / (2 ** (len(indiv) / 2) - 1)) * (limit_two - limit_one) + limit_one
+    float_individ_two = (int_individ_two / (2 ** (len(indiv) / 2) - 1)) * (limit_two - limit_one) + limit_one
+
+    return float_individ_two, float_individ_one
+
+
 # >--------------------------------------------------------------------------------------------------------
 
-for i in range(individ):
-    num_child = []
+for i in range(count_individ):
+    num_individ = []
     for i in range(len_individ):
         a = randint(0, 1)
-        num_child.append(a)
-    population.append(num_child)
+        num_individ.append(a)
+    population.append(num_individ)
 
 
 
 for i in population:
-    population_fit.append(fitness(i))
+    population_fit.append(fitness_one(i))
 
 
 for i in range(len(population_fit)):
@@ -1074,12 +1092,12 @@ for i in range(len(population_fit)):
     temp_count_fit += population_fit[i]
 
 best_individ.append(population[index_])
-count_str.append(temp_count_fit / individ)
-best_p.append(best)
+count_str.append(temp_count_fit / count_individ)
+best_individ_array.append(best)
 best = -math.inf
 
 # >--------------------------------------------------------------------------------------------------------
-for i in range((steps - 1) * individ):  # Основной цикл создания популяций
+for i in range((count_generations - 1) * count_individ):  # Основной цикл создания популяций
     #  while countc < len_individ:
 # >--------------------------------------------------------------------------------------------------------
     fp = randint(0, len(population) - 1)  # Турнирный метод отбора родителей
@@ -1108,33 +1126,33 @@ for i in range((steps - 1) * individ):  # Основной цикл создан
 
     for i in range(len(child)):  # Метод мутации
         rand = random()
-        if rand <= mutation_veriety:
+        if rand <= mutation_probability:
             child[i] = 1 if child[i] == 0 else 0
-    lst_fit_temp.append(fitness(child))
-    sep_population.append(child)  # Добавление ребенка в список
+    lst_fit_temp.append(fitness_one(child))
+    child_population.append(child)  # Добавление ребенка в список
 
 # >--------------------------------------------------------------------------------------------------------
     best = -math.inf
-    if len(population) == len(sep_population):
+    if len(population) == len(child_population):
         for i in range(len(population_fit)):
             if best <= lst_fit_temp[i]:
                 best = lst_fit_temp[i]
                 index_ = i
             temp_count_fit += lst_fit_temp[i]
 
-        best_p.append(best)
-        count_str.append(temp_count_fit / individ)
-        best_individ.append(sep_population[index_])
+        best_individ_array.append(best)
+        count_str.append(temp_count_fit / count_individ)
+        best_individ.append(child_population[index_])
 
         population_fit = lst_fit_temp.copy()
-        population = sep_population.copy()
-        sep_population = []
+        population = child_population.copy()
+        child_population = []
         lst_fit_temp = []
         best = -math.inf
     temp_count_fit = 0
     #  print(population, "популяция")
 
-    print(population)
+    # print(population)
 
 # >--------------------------------------------------------------------------------------------------------
 
@@ -1143,22 +1161,56 @@ for ex in range(len(count_str) + 1):
 
 
 for i in best_individ:
-    best_individ_x.append(fitness_x(i))
+    best_individ_x_one.append(float_number(i)[0])
+for i in best_individ:
+    best_individ_x_two.append(float_number(i)[1])
+
+
 
 
 # >--------------------------------------------------------------------------------------------------------
-print(count_str, 'count str ')
-print(best_individ, 'best individ')
-print(child, 'child')
+# print(count_str, 'count str ')
+# print(best_individ, 'best_y count_individ')
+# print(child, 'child')
 # >--------------------------------------------------------------------------------------------------------
-figure, axis = plt.subplots()
-x_one = np.linspace(limit_one, limit_two, 50)
-y_one = [fi(i) for i in x_one]
-x = best_individ_x
-y = best_p
+# figure, axis = plt.subplots()
 
-plt.scatter(x, y, 20)
-axis.plot(x_one, y_one)
+#
+# axis = figure.add_subplot(111, projection='3d')
+# x = best_individ_x_one
+# y = best_individ_array
+# z = best_individ_x_two
+#
+# x1 = np.linspace(limit_one, limit_two, 50)
+# y1 = np.linspace(limit_one, limit_two, 50)
+# z1 = [function(i, k) for i, k in zip(x1, y1)]
+# x1, y1 = np.meshgrid(x1, y1)
+# axis.plot_surface(x1, y1, z1, color="red")
+#
+# axis.scatter(x, y, z)
+# plt.show()
+
+# x_one = np.linspace(limit_one, limit_two, 50)
+# y_one = [function(i) for i in x_one]
+# x = best_individ_x_one
+# y = best_individ_array
+#
+# plt.scatter(x, y, 20)
+# axis.plot(x_one, y_one)
+# plt.show()
+# print(x , 'x')
+# print(y, 'y')
+
+# fig = plt.figure()
+# ax = fig.add_subplot(111, projection='3d')
+
+fig, ax = plt.subplots(subplot_kw={'projection':'3d'})
+
+x1 = best_individ_x_one
+y1 = best_individ_x_two
+z1 = best_individ_array
+plt.scatter(x1, z1, y1, c='b')
+
+print(population)
+
 plt.show()
-print(x , 'x')
-print(y, 'y')
